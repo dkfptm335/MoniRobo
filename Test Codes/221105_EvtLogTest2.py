07d4e2140ca6a0d11d0ae494ca319a084e759a7d
jhs3549 <michael3549@naver.com>
2022-11-05 22:22:08 +0900

    Test for evtlog include HRESULT_CODE in winerror

---
+++ Test Codes/221105_EvtLogTest2.py
@@ -0,0 +1,48 @@
+from http import server
+import win32evtlog
+import win32con
+import unicodedata
+import win32evtlogutil
+import win32security
+import winerror
+
+# 이벤트로그 중 System 타입의 이벤트로그를 출력
+evtH = win32evtlog.OpenEventLog(None, "System")
+
+Event_Types = {
+    1 : "\"Error\"",
+    2 : "\"Caution\"",
+    4 : "\"Information\"",
+    8 : "\"Success Audit\"",
+    16 : "\"Failure Audit\"" 
+} # Event_Types 출력 칸수 조절
+
+numEvt = win32evtlog.GetNumberOfEventLogRecords(evtH)
+print("Total Number of EventLogRecords:", numEvt, "\n")
+
+objects = win32evtlog.ReadEventLog(evtH, win32evtlog.EVENTLOG_BACKWARDS_READ|win32evtlog.EVENTLOG_SEQUENTIAL_READ, 0, 8192)
+
+# 각 Property별로 칸 만들기
+# print("Record# | TimeGenerated | ComputerName | EventType | SourceName\n")
+
+for object in objects:
+    #print("[" + str(object.RecordNumber) + "]", object.TimeGenerated, object.EventID, object.EventCategory, object.ComputerName, Event_Types.get(object.EventType, "Unknown"), object.SourceName)
+    # print(object.Sid)
+    evtid = str(winerror.HRESULT_CODE(object.EventID))
+    print(evtid)
+
+    if object.Sid is not None:
+        domain, user, typ = win32security.LookupAccountSid(None, object.Sid)
+        sidDesc = "%s/%s" % (domain, user)
+    #print(sidDesc)
+
+
+    # 각 log별 세부정보를 어떤 방식으로 출력할까? 그냥 보여주기 or 선택한 부분만 보여주기
+    msg = win32evtlogutil.SafeFormatMessage(object, "System")
+    if msg[0:8] != "<The des":
+        print(msg)
+    else:
+        print()
+    # msg.update({object.RecordNumber:str(win32evtlogutil.SafeFormatMessage(object, "Application"))})
+    # print(str(win32evtlogutil.SafeFormatMessage(object, "System")))
+    # print("\n")
